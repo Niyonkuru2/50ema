1bc5d23c3d6ebffd4ff17a0393c73276bac3bd9c
Niyonkuru2 <150118511+Niyonkuru2@users.noreply.github.com>
2026-02-19 05:56:50 +0200

    Change cron job schedule from 15 to 5 minutes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.listen(PORT, () => {
 });
 
  //Run automatic analysis every 15 minutes
-cron.schedule("*/15 * * * *", async () => {
+cron.schedule("*/5 * * * *", async () => {
  console.log("Running scheduled market analysis...");
    await autoAnalyzeMarket();
 });
